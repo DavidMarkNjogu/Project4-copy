d8aa19dadcb52f6f4873bc6b2f1517df4f5fb2d1
DavidMarkNjogu <davidmarknjogu@gmail.com>
2024-12-24 23:59:09 +0300

    Add path for TemplateView for the base.html

--- realestate/urls.py
+++ realestate/urls.py
@@ -18,10 +18,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('listings/', include('listings.urls')),
+    path('', TemplateView.as_view(template_name='home/base.html'), name='home'),
+    # Home page # Serve index.html at the root URL
 ]
 
 if settings.DEBUG:
