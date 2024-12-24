7201f41cc761b2a54c5a883320416fbbbeceffa4
DavidMarkNjogu <davidmarknjogu@gmail.com>
2024-12-24 23:56:59 +0300

    Remove views.{{ function }} in urlpatterns[ Change import from . import vies to from . views import the {{"relevant function"}}

--- listings/urls.py
+++ listings/urls.py
@@ -1,9 +1,10 @@
 # listings/urls.py
 
 from django.urls import path
-from . import views
+from .views import listings_list, listing_detail
 
 urlpatterns = [
-    path('', views.listings, name='listings'),
-    path('<int:listing_id>/', views.listing_detail, name='listing_detail'),
+    path('',listings_list, name='listings'),
+    path('<int:listing_id>/',listing_detail, name='listing_detail'),
+
 ]
