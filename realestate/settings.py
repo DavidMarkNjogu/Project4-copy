b6521a164bd5cab6a10d68c07de62aafc1a0b534
DavidMarkNjogu <davidmarknjogu@gmail.com>
2024-12-24 23:47:35 +0300

    Rerouted the position of the media setting

--- realestate/settings.py
+++ realestate/settings.py
@@ -72,8 +72,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'realestate.wsgi.application'
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
@@ -126,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
