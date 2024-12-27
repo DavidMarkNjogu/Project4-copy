6d3ada59f7e8b28c77571602037268ab3fcf7664
DavidMarkNjogu <davidmarknjogu@gmail.com>
2024-12-27 17:21:21 +0300

    Add bookings URL configuration to main urlpatterns

--- realestate/urls.py
+++ realestate/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path("accounts/", include("accounts.urls")), 
     path('accounts/',include('django.contrib.auth.urls')),  # Django authentication URLs
     path('', TemplateView.as_view(template_name='home.html'), name='home'),# Home page # Serve index.html at the root URL
+    path('bookings/', include('bookings.urls')),  # Include bookings URLs
 ]
 
 if settings.DEBUG:
