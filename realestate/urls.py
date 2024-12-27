20cbdeaf052dbd58e8d8f370c4ac9e9dc2f86c08
DavidMarkNjogu <davidmarknjogu@gmail.com>
2024-12-27 06:09:38 +0300

    Update home template path by changing the path from   path('', TemplateView.as_view(template_name='home/base.html'), name='home'), to  path('', TemplateView.as_view(template_name='base.html'), name='home'),

--- realestate/urls.py
+++ realestate/urls.py
@@ -23,7 +23,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('listings/', include('listings.urls')),
-    path('', TemplateView.as_view(template_name='home/base.html'), name='home'),
+    path('', TemplateView.as_view(template_name='base.html'), name='home'),
     path('accounts/',include('django.contrib.auth.urls')),  # Django authentication URLs
     # Home page # Serve index.html at the root URL
 ]
