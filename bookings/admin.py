4aa92d992d6c006ce773ad9048699c30029b3330
DavidMarkNjogu <davidmarknjogu@gmail.com>
2024-12-27 14:41:27 +0300

    Register Booking model in admin for management

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,5 @@
-from django.contrib import admin
+from django.contrib import admin  # Importing the admin module
+from .models import Booking  # Importing the Booking model
 
-# Register your models here.
+# Registering the Booking model to make it accessible in the admin interface
+admin.site.register(Booking)
