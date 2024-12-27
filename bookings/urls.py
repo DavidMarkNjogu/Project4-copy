c3a9ef39956d62e993bdc9c9fafcd13ffa45ea83
DavidMarkNjogu <davidmarknjogu@gmail.com>
2024-12-27 18:11:49 +0300

    Update booking URL to use book_listing instead of   booking_property function for consistency

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path  # Importing path for URL routing
-from .views import book_property  # Importing the booking view
+from .views import book_listing  # Importing the booking view
 
 urlpatterns = [
-    path('book/', book_property, name='book_form'),  # URL for the booking form
+    path('book/', book_listing, name='book_form'),  # URL for the booking form
 ]
