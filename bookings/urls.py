c7dedde82cf36a3419ab4f025e5a2db7d6f37459
DavidMarkNjogu <davidmarknjogu@gmail.com>
2024-12-27 18:21:42 +0300

    Import booking_success view for URL routing

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path  # Importing path for URL routing
-from .views import book_listing  # Importing the booking view
+from .views import book_listing, booking_success  # Importing the booking view
 
 urlpatterns = [
     path('book/', book_listing, name='book_form'),  # URL for the booking form
