89e3a9eee08750da01ad75baebc1a5bfdbfbf7d6
DavidMarkNjogu <davidmarknjogu@gmail.com>
2024-12-27 17:42:14 +0300

    Import PropertyListing model in bookings views for enhanced functionality

--- bookings/views.py
+++ bookings/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect  # Importing render and redirect functions
 from django.contrib import messages  # Importing messages for notifications
+
+from listings.models import PropertyListing
 from .models import Booking  # Importing the Booking model
 from django.core.exceptions import ValidationError  # Importing ValidationError for handling validation errors
 
