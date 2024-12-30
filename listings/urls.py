4d08685b0ed01c7256771279dd68139098459ea6
DavidMarkNjogu <davidmarknjogu@gmail.com>
2024-12-30 22:48:20 +0300

    Change url rerouting

--- listings/urls.py
+++ listings/urls.py
@@ -6,7 +6,7 @@ from .views import listings_list, listing_detail
 app_name = 'listings'
 
 urlpatterns = [
-    path('',listings_list, name='listings'),
+    path('',listings_list, name='listings_list'),
     path('<int:listing_id>/',listing_detail, name='listing_detail'),
 
 ]
