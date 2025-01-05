4545f7ad3ad01c046154318b9601c20ac50460b9
DavidMarkNjogu <davidmarknjogu@gmail.com>
2025-01-05 20:19:52 +0300

    Add listing update and delete functionality with corresponding templates and views

--- listings/urls.py
+++ listings/urls.py
@@ -1,12 +1,15 @@
 # listings/urls.py
 
 from django.urls import path
-from .views import listings_list, listing_detail
+from .views import listings_list, listing_detail, listing_create, listing_update, listing_delete
 
 app_name = 'listings'
 
 urlpatterns = [
     path('',listings_list, name='listings_list'),
     path('<int:listing_id>/',listing_detail, name='listing_detail'),
+    path('create/',listing_create, name='listing_create'),
+    path('update/<int:listing_id>/',listing_update, name='listing_update'),
+    path('delete/<int:listing_id>/',listing_delete, name='listing_delete'),
 
 ]
--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import PropertyListing
 from .forms import PropertyListingForm
 
@@ -12,10 +12,30 @@ def listing_detail(request, listing_id):
 
 def listing_create(request):
     if request.method == 'POST':
-        form = PropertyListingForm()
+        form = PropertyListingForm(request.POST, request.FILES) 
         if form.is_valid():
             form.save()
             return redirect('listings:listings_list')
-    else:
+    else: 
         form = PropertyListingForm()
     return render(request, 'listings/listing_create.html', {'form': form})
+
+def listing_update(request, listing_id):
+    listing = get_object_or_404(PropertyListing, id=listing_id)
+    form = PropertyListingForm(instance=listing)
+
+    if request.method == 'POST':
+        form = PropertyListingForm(request.POST, files=request.FILES,  instance=listing)
+        if form.is_valid():
+            form.save()
+            return redirect('listings:listing_detail', listing_id=listing_id)
+    
+    else:
+        return render(request, 'listings/listing_update.html', {'form': form})
+
+def listing_delete(request, listing_id):
+    listing = get_object_or_404(PropertyListing, id=listing_id)
+    if request.method == 'POST':
+        listing.delete()
+        return redirect('listings:listings_list')
+    
