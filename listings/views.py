4f9ac7208660d2cdd395f2bfa4af6e0989f86e51
DavidMarkNjogu <davidmarknjogu@gmail.com>
2025-01-05 15:17:50 +0300

    Add listing_create view for property listing creation

--- listings/views.py
+++ listings/views.py
@@ -1,7 +1,6 @@
-# listings/views.py
-
 from django.shortcuts import render, get_object_or_404
 from .models import PropertyListing
+from .forms import PropertyListingForm
 
 def listings_list(request):
     listings = PropertyListing.objects.all()
@@ -11,3 +10,12 @@ def listing_detail(request, listing_id):
     listing = get_object_or_404(PropertyListing, pk=listing_id)
     return render(request, 'listings/listing_detail.html', {'listing': listing})
 
+def listing_create(request):
+    if request.method == 'POST':
+        form = PropertyListingForm()
+        if form.is_valid():
+            form.save()
+            return redirect('listings:listings_list')
+    else:
+        form = PropertyListingForm()
+    return render(request, 'listings/listing_create.html', {'form': form})
